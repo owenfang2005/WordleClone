9f613c69aeef7bf512d5bd16e220680315e30951
Fang <Owen.Fang@ontario.ca>
2024-07-04 11:05:05 -0400

    Lesson 10

--- my-app/src/components/Grid.js
+++ my-app/src/components/Grid.js
@@ -5,6 +5,9 @@ export default function Grid({ currentGuess, guesses, turn }) {
   return (
     <div>
         {guesses.map((g, i) => {
+            if (turn === i) {
+                return <Rows key={i} currentGuess={currentGuess} />
+            }
             return <Rows key={i} guess={g}/>
         })}
     </div>
