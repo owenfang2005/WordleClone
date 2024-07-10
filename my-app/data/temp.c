fa9251a72dfc50291d6e56baa85cd8e27f9d3226
Fang <Owen.Fang@ontario.ca>
2024-07-10 10:42:10 -0400

    - Updated script to include a display element which houses upper case letters - Key element houses lowercase letters - These changes were made for usedKeys function where inputted keys were lowercase but only letters stored in database were uppercase letters - Uppercase letters are stored for display

--- my-app/data/temp.c
+++ my-app/data/temp.c
@@ -3,14 +3,20 @@
 #include <stdlib.h>
 
 int main(void) {
-    char alphabet[26];
+    char lowAlphabet[26]; // for uppercase letter
+    char upAlphabet[26]; // for lowercase letter
     printf("Input: ");
     for (int i = 0; i < 26; i++) {
         char temp;
         scanf("%c", &temp);
-        alphabet[i] = temp;
+        lowAlphabet[i] = temp + 32;
+        upAlphabet[i] = temp;
     }
     for (int i = 0; i < 26; i++) {
-        printf("\'%c\', ", alphabet[i]);
+        printf("\'%c\', ", lowAlphabet[i]);
+    }
+    printf("\n\n");
+    for (int i = 0; i < 26; i++) {
+        printf("\'%c\', ", upAlphabet[i]);
     }
 }
