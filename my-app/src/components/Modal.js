3b6ae466c2f4e64ac1a69ba30e3014da3ed8fd1d
Fang <Owen.Fang@ontario.ca>
2024-07-11 16:01:57 -0400

    - made modal look better - added play again button (non functional)

--- my-app/src/components/Modal.js
+++ my-app/src/components/Modal.js
@@ -8,6 +8,9 @@ export default function Modal({ isCorrect, turn, solution }) {
                 <h1>You win!</h1>
                 <p className="solution">Solution: {solution}</p>
                 <p>You found the solution in {turn} guesses.</p>
+                <div className="playAgain">
+                    <p className="pa">PLAY AGAIN!</p>
+                </div>
             </div>
         )}
         {!isCorrect && (
@@ -15,7 +18,11 @@ export default function Modal({ isCorrect, turn, solution }) {
                 <h1>You lose.</h1>
                 <p className="solution">Solution: {solution}</p>
                 <p>Better luck next time! </p>
+                <div className="playAgain">
+                    <p className="pa">PLAY AGAIN!</p>
+                </div>
             </div>
+            
         )}
     </div>
   )
